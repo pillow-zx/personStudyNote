c139c8a7097974b20ab2c04bea92c929c832b0c9
waysorry <waysorrypub@gmail.com>
2025-03-30 00:36:28 +0800

    整理仓库

--- file/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-vim.g.autoformat = false
-
-vim.g.neovide_autoformat = false
--- 设置 Neovide 缩放比例
-vim.g.neovide_scale_factor = 0.9
